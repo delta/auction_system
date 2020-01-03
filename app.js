a2ef46ee1c1a448cf54fbfe584be582368d4a171
shakti97 <shaktisingh230797@gmail.com>
2020-01-04 00:23:50 +0530

    remove redundant code, fix typo, migrate bidHandler

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const db= require('./models/index')
+const db = require('./models/index');
 //routes
 const routes = require('./src/routes');
-
 
 //assets
 app.use('/assets', express.static('assets'));
 app.use('/build', express.static('public/build'));
 
-
 const WEBPACK_ASSETS_URL = 'http://localhost:8080';
 
-if(app.get('env') != 'development'){
-	var assets_names = JSON.parse(fs.readFileSync(__dirname+ '/assets/webpack-assets.json', 'utf8'));
-	var scripts = [assets_names.commonChunk.js,assets_names.app.js];
-}
-else{
-	var scripts = [(WEBPACK_ASSETS_URL + '/build/app.js')];
+if (app.get('env') != 'development') {
+    var assets_names = JSON.parse(fs.readFileSync(__dirname + '/assets/webpack-assets.json', 'utf8'));
+    var scripts = [assets_names.commonChunk.js, assets_names.app.js];
+} else {
+    var scripts = [WEBPACK_ASSETS_URL + '/build/app.js'];
 }
 
 //Set view engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-
-//use routes with /api 
+//use routes with /api
 app.use('/', routes);
-
 
 //Allow react router to handle the get requests
 app.get('/*', function(req, res) {
-	res.render('index',{ scripts: scripts });
+    res.render('index', {scripts: scripts});
 });
 
-
-const server = app.listen(4000, function () {
-	console.log('App listening on port 4000!');
+const server = app.listen(4000, function() {
+    console.log('App listening on port 4000!');
 });
-db.sequelize.sync().then(()=>{
-	console.log('DB Synced')
-})
+db.sequelize.sync({
+    force: true
+}).then(() => {
+    console.log('DB Synced');
+});
 //socket server
 const io = require('socket.io')(server, {wsEngine: 'ws'});
 
@@ -48,16 +44,16 @@ const handleConnections = require('./src/socketRoutes/connectionManager');
 
 //socket routes
 io.sockets.on('connection', socket => {
-	socket.on('openAuction', (namespace, owner_id) => {
-		handleConnections.ownerSokcet(socket, namespace, owner_id);
-	});
-	socket.on('closeAuction', (namespace, owner_id) => {
-		handleConnections.closeAuction(socket, io, namespace, owner_id);
-	});
-	socket.on('joinRoom', (namespace, user_id) => {
-		handleConnections.joinAuction(socket, namespace, user_id);
-	});
-	socket.on('newBid', (namespace, user_id, userName, bid_value) => {
-		handleConnections.handleBid(socket, io, namespace, user_id, userName, bid_value);
-	});
+    socket.on('openAuction', (namespace, owner_id) => {
+        handleConnections.ownerSocket(socket, namespace, owner_id);
+    });
+    socket.on('closeAuction', (namespace, owner_id) => {
+        handleConnections.closeAuction(socket, io, namespace, owner_id);
+    });
+    socket.on('joinRoom', (namespace, user_id) => {
+        handleConnections.joinAuction(socket, namespace, user_id);
+    });
+    socket.on('newBid', (namespace, user_id, userName, bid_value) => {
+        handleConnections.handleBid(socket, io, namespace, user_id, userName, bid_value);
+    });
 });
--- src/socketRoutes/bidManager.js
+++ src/socketRoutes/bidManager.js
@@ -1,2 +1,47 @@
 const Sequelize = require('sequelize');
 const models = require(__dirname + '/../../models/');
+
+let bidDetails = {}; // {"room1": {currentBid: 34, bidHolderId: 1}, "room2": {currentBid: 21, bidHolderId: 9}} current bidValues in each room
+
+//creating a Bid
+function creatingBid(namespace) {
+    bidDetails[namespace] = {
+        currentBid: 0,
+        bidHolderId: -1,
+        bidHolderName: '-'
+    };
+    return;
+}
+
+//delete bid
+function deleteBid(namespace) {
+    delete bidDetails[namespace];
+    return;
+}
+
+//show currentBids
+function showCurrentBid(socket, namespace) {
+    //send current bidDetails to this newly added client
+    socket.emit('currentBidStatus', bidDetails[namespace]);
+    return;
+}
+
+//handle bids for different auctions
+function handleBid(io, namespace, user_id, userName, bid_value) {
+    //update bidDetails
+    bidDetails[namespace].currentBid = bid_value;
+    bidDetails[namespace].bidHolderId = user_id;
+    bidDetails[namespace].bidHolderName = userName;
+
+    //brodcast updated bid to all clients in the room
+    io.sockets.in(namespace).emit('currentBidStatus', bidDetails[namespace]);
+    return;
+}
+
+module.exports = {
+    creatingBid,
+    deleteBid,
+    showCurrentBid,
+    handleBid,
+    bidDetails
+};
